9f2bd3cafb091fc089aa70080ceb40dfcb811cc7
Shubham Prajapati <89298156+p-shubh@users.noreply.github.com>
2024-07-30 15:24:31 +0000

    adding new column in avatar as avatar voice

--- models/avatar.go
+++ models/avatar.go
@@ -8,14 +8,15 @@ import (
 )
 
 type Avatar struct {
-	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
-	AvatarID   string    `json:"avatar_id"`
-	URL        string    `json:"url"`
-	UserID     string    `json:"user_id"`
-	PhygitalID string    `json:"phygital_id"`
-	ChaintypeID  		uuid.UUID	   `gorm:"type:uuid" json:"chaintype_id"`
-	CreatedAt  time.Time `gorm:"type:timestamp;default:current_timestamp" json:"created_at"`
-	UpdatedAt  time.Time `gorm:"type:timestamp;default:current_timestamp" json:"updated_at"`
+	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
+	AvatarID    string    `json:"avatar_id"`
+	URL         string    `json:"url"`
+	UserID      string    `json:"user_id"`
+	PhygitalID  string    `json:"phygital_id"`
+	AvatarVoice string    `json:"avatar_voice"`
+	ChaintypeID uuid.UUID `gorm:"type:uuid" json:"chaintype_id"`
+	CreatedAt   time.Time `gorm:"type:timestamp;default:current_timestamp" json:"created_at"`
+	UpdatedAt   time.Time `gorm:"type:timestamp;default:current_timestamp" json:"updated_at"`
 }
 
 func (a *Avatar) BeforeCreate(tx *gorm.DB) (err error) {
