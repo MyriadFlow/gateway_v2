1f4d16f0ceb9d4018c8d9392fc676e2b7ca55f7a
Shubham Prajapati <shubhamprajapati032@gmail.com>
2025-02-11 22:36:53 +0530

    updating model

--- models/brand.go
+++ models/brand.go
@@ -7,49 +7,50 @@ import (
 	"gorm.io/gorm"
 )
 
-
 type Brand struct {
-	ID              uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
-	Name            string    `json:"name"`
-	AgentId			string	  `json:"agent_id"`
-	Slogan          string    `json:"slogan"`
-	Description     string    `json:"description"`
-	LogoImage       string    `json:"logo_image"`
-	CoverImage      string    `json:"cover_image"`
-	Representative  string    `json:"representative"`
-	ContactEmail    string    `json:"contact_email"`
-	ContactPhone    string    `json:"contact_phone"`
-	ShippingAddress string    `json:"shipping_address"`
-	Website         string    `json:"website"`
-	Twitter         string    `json:"twitter"`
-	Instagram       string    `json:"instagram"`
-	Facebook        string    `json:"facebook"`
-	Telegram        string    `json:"telegram"`
-	LinkedIn        string    `json:"linkedin"`
-	Youtube        string    `json:"youtube"`
-	Discord        string    `json:"discord"`
-	Whatsapp        string    `json:"whatsapp"`
-	Google          string     `json:"google"`
-	Tiktok          string     `json:"tiktok"`
-	Snapchat          string     `json:"snapchat"`
-	Pinetrest          string     `json:"pinetrest"`
-	AdditionalLink  string    `json:"additional_link"`
-	Link            string    `json:"link"`
-	AdditionalInfo  string    `json:"additional_info"`
-	Industry        string    `json:"industry"`
-	Tags            string    `json:"tags"`
-	Fees            float64   `json:"fees" gorm:"type:decimal(20,10)"`
-	PayoutAddress   string    `json:"payout_address"`
-	AccessMaster    string    `json:"access_master"`
-	TradeHub        string    `json:"trade_hub"`
-	Blockchain      string    `json:"blockchain"`
-	ChainID         string    `json:"chain_id"`
-	ChaintypeID     uuid.UUID `gorm:"type:uuid" json:"chaintype_id"`
-	ManagerID       string    `json:"manager_id"` //user walletaddress
-	ElevateRegion   string    `json:"elevate_region"`
-	WebXRExperienceWithAiAvatar bool `json:"webxr_experience_with_ai_avatar"`
-	CreatedAt       time.Time `gorm:"type:timestamp;default:current_timestamp" json:"created_at"`
-	UpdatedAt       time.Time `gorm:"type:timestamp;default:current_timestamp" json:"updated_at"`
+	ID                          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
+	Name                        string    `json:"name"`
+	AgentId                     string    `json:"agent_id"`
+	Slogan                      string    `json:"slogan"`
+	Description                 string    `json:"description"`
+	LogoImage                   string    `json:"logo_image"`
+	CoverImage                  string    `json:"cover_image"`
+	Representative              string    `json:"representative"`
+	ContactEmail                string    `json:"contact_email"`
+	ContactPhone                string    `json:"contact_phone"`
+	ShippingAddress             string    `json:"shipping_address"`
+	Website                     string    `json:"website"`
+	Twitter                     string    `json:"twitter"`
+	Instagram                   string    `json:"instagram"`
+	Facebook                    string    `json:"facebook"`
+	Telegram                    string    `json:"telegram"`
+	LinkedIn                    string    `json:"linkedin"`
+	Youtube                     string    `json:"youtube"`
+	Discord                     string    `json:"discord"`
+	Whatsapp                    string    `json:"whatsapp"`
+	Google                      string    `json:"google"`
+	Tiktok                      string    `json:"tiktok"`
+	Snapchat                    string    `json:"snapchat"`
+	Pinetrest                   string    `json:"pinetrest"`
+	AdditionalLink              string    `json:"additional_link"`
+	Link                        string    `json:"link"`
+	AdditionalInfo              string    `json:"additional_info"`
+	Industry                    string    `json:"industry"`
+	Tags                        string    `json:"tags"`
+	Fees                        float64   `json:"fees" gorm:"type:decimal(20,10)"`
+	PayoutAddress               string    `json:"payout_address"`
+	AccessMaster                string    `json:"access_master"`
+	TradeHub                    string    `json:"trade_hub"`
+	Blockchain                  string    `json:"blockchain"`
+	ChainID                     string    `json:"chain_id"`
+	ChaintypeID                 uuid.UUID `gorm:"type:uuid" json:"chaintype_id"`
+	ManagerID                   string    `json:"manager_id"` //user walletaddress
+	ElevateRegion               string    `json:"elevate_region"`
+	WebXRExperienceWithAiAvatar bool      `json:"webxr_experience_with_ai_avatar"`
+	Image360                    string    `json:"image360"`
+	Video360                    string    `json:"video360"`
+	CreatedAt                   time.Time `gorm:"type:timestamp;default:current_timestamp" json:"created_at"`
+	UpdatedAt                   time.Time `gorm:"type:timestamp;default:current_timestamp" json:"updated_at"`
 }
 
 func (b *Brand) BeforeCreate(tx *gorm.DB) (err error) {
